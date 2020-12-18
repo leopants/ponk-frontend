36773396da5973076b85844d51185a30e4908d64
reygranda <reygdev@gmail.com>
2020-12-18 15:26:37 -0500

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
+import Header from './components/Header/Header';
+
 
 function App() {
   return (
-    
     <div>
-      <SignupForm />
-      <LoginForm />
+      <Header />
     </div>
   );
 }
 
 export default App;
+
