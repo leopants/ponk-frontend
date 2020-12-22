a77b137ab17f3af75aabb8a75f6eb277cce236d0
Leonardo Pantaleon <lpant011@fiu.edu>
2020-12-21 19:49:13 -0500

    added login redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Form, Button, Col, Row, Container } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignupForm from "./components/Signupform/SignupForm";
 import LoginForm from "./components/Loginform/LoginForm";
 import Header from "./components/Header/Header";
@@ -9,39 +10,46 @@ import Calltext from "./components/CallText/Calltext";
 function App() {
     return (
         <div>
-            <Container fluid>
-                <Row>
-                    <Col>
-                        <Header />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col
-                        sm={{ span: 6, offset: 3 }}
-                        xs={{ span: 6, offset: 3 }}
-                    >
-                        <Calltext />
-                    </Col>
-                </Row>
-                <Row className="formRow">
-                    <Col
-                        sm={{ span: 6, offset: 3 }}
-                        md={{ span: 6, offset: 3 }}
-                        lg={{ span: 3, offset: 3 }}
-                        xs={{ span: 8, offset: 2 }}
-                    >
-                        <LoginForm />
-                    </Col>
-                    <Col
-                        sm={{ span: 6, offset: 3 }}
-                        md={{ span: 6, offset: 3 }}
-                        lg={{ span: 3, offset: 0 }}
-                        xs={{ span: 8, offset: 2 }}
-                    >
-                        <SignupForm />
-                    </Col>
-                </Row>
-            </Container>
+            <Switch>
+                <Route path="/" exact>
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <Header />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col
+                                sm={{ span: 6, offset: 3 }}
+                                xs={{ span: 8, offset: 2 }}
+                            >
+                                <Calltext />
+                            </Col>
+                        </Row>
+                        <Row className="formRow">
+                            <Col
+                                sm={{ span: 6, offset: 3 }}
+                                md={{ span: 6, offset: 3 }}
+                                lg={{ span: 3, offset: 3 }}
+                                xs={{ span: 8, offset: 2 }}
+                            >
+                                <LoginForm />
+                            </Col>
+                            <Col
+                                sm={{ span: 6, offset: 3 }}
+                                md={{ span: 6, offset: 3 }}
+                                lg={{ span: 3, offset: 0 }}
+                                xs={{ span: 8, offset: 2 }}
+                            >
+                                <SignupForm />
+                            </Col>
+                        </Row>
+                    </Container>
+                </Route>
+                <Route path="/dashboard">
+                    <h1>Dashboard</h1>
+                </Route>
+            </Switch>
         </div>
     );
 }
--- src/components/Loginform/LoginForm.js
+++ src/components/Loginform/LoginForm.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { Form, Button, Col, Row, Container, InputGroup } from "react-bootstrap";
+import {
+    Form,
+    Button,
+    Col,
+    Row,
+    Container,
+    InputGroup,
+    useState,
+} from "react-bootstrap";
 import { PersonFill, KeyFill, PersonCircle } from "react-bootstrap-icons";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+    useHistory,
+} from "react-router-dom";
 import "./Loginform.css";
 const axios = require("axios");
 
 export default function LoginForm() {
     const [usernameOrEmail, setusernameOrEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
+    const [loggedIn, setLogin] = React.useState(false);
+    const history = useHistory();
 
     const handleSubmit = (event) => {
-        console.log(usernameOrEmail);
-        console.log(password);
         try {
             axios
                 .get("https://ponk-backend.herokuapp.com/checkuser", {
@@ -21,7 +36,8 @@ export default function LoginForm() {
                 })
                 .then(function (response) {
                     if (response.request.status == 200) {
-                        alert("COCK AND BALLS");
+                        console.log("cock and balls");
+                        history.push("/dashboard");
                     }
                 });
         } catch (err) {
@@ -34,79 +50,83 @@ export default function LoginForm() {
 
     return (
         <div className="loginDiv">
-            <Container fluid>
-                <Row>
-                    <Col xs={12}>
-                        <Form className="loginForm" onSubmit={handleSubmit}>
-                            <h1 className="loginText">Login</h1>
-                            <Row>
-                                <Col xs={{ span: 10, offset: 1 }}>
-                                    <Form.Group controlId="formBasicEmail">
-                                        <InputGroup>
-                                            <InputGroup.Prepend>
-                                                <InputGroup.Text>
-                                                    <PersonCircle />
-                                                </InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                            <Form.Control
-                                                value={usernameOrEmail}
-                                                name="usernameOrEmail"
-                                                type="text"
-                                                placeholder="Username or email"
-                                                onChange={(e) =>
-                                                    setusernameOrEmail(
-                                                        e.target.value
-                                                    )
-                                                }
-                                                required
-                                            />
-                                        </InputGroup>
-                                    </Form.Group>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col xs={{ span: 10, offset: 1 }}>
-                                    <Form.Group controlId="formBasicPassword">
-                                        <InputGroup>
-                                            <InputGroup.Prepend>
-                                                <InputGroup.Text>
-                                                    <KeyFill />
-                                                </InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                            <Form.Control
-                                                name="password"
-                                                value={password}
-                                                type="password"
-                                                placeholder="Password"
-                                                onChange={(e) =>
-                                                    setPassword(e.target.value)
-                                                }
-                                                required
-                                            />
-                                        </InputGroup>
-                                    </Form.Group>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col xs={{ span: 10, offset: 1 }}>
-                                    <Form.Group
-                                        style={{ alignItems: "center" }}
-                                    >
-                                        <Button
-                                            block
-                                            className="loginButton"
-                                            variant="primary"
-                                            type="submit"
+            <Switch>
+                <Container fluid>
+                    <Row>
+                        <Col xs={12}>
+                            <Form className="loginForm" onSubmit={handleSubmit}>
+                                <h1 className="loginText">Login</h1>
+                                <Row>
+                                    <Col xs={{ span: 10, offset: 1 }}>
+                                        <Form.Group controlId="formBasicEmail">
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Text>
+                                                        <PersonCircle />
+                                                    </InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                                <Form.Control
+                                                    value={usernameOrEmail}
+                                                    name="usernameOrEmail"
+                                                    type="text"
+                                                    placeholder="Username or email"
+                                                    onChange={(e) =>
+                                                        setusernameOrEmail(
+                                                            e.target.value
+                                                        )
+                                                    }
+                                                    required
+                                                />
+                                            </InputGroup>
+                                        </Form.Group>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col xs={{ span: 10, offset: 1 }}>
+                                        <Form.Group controlId="formBasicPassword">
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Text>
+                                                        <KeyFill />
+                                                    </InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                                <Form.Control
+                                                    name="password"
+                                                    value={password}
+                                                    type="password"
+                                                    placeholder="Password"
+                                                    onChange={(e) =>
+                                                        setPassword(
+                                                            e.target.value
+                                                        )
+                                                    }
+                                                    required
+                                                />
+                                            </InputGroup>
+                                        </Form.Group>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col xs={{ span: 10, offset: 1 }}>
+                                        <Form.Group
+                                            style={{ alignItems: "center" }}
                                         >
-                                            Submit
-                                        </Button>
-                                    </Form.Group>
-                                </Col>
-                            </Row>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
+                                            <Button
+                                                block
+                                                className="loginButton"
+                                                variant="primary"
+                                                type="submit"
+                                            >
+                                                Submit
+                                            </Button>
+                                        </Form.Group>
+                                    </Col>
+                                </Row>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </Switch>
         </div>
     );
 }
